4b822f023d14e8458b2fa1628cc12289210baacc
theprogramer <theprogramer@theprogramer.com.br>
2014-01-30 15:47:47 -0200

    Ajustando logotipo

--- src/header.php
+++ src/header.php
@@ -14,10 +14,7 @@
 		    <div class="row">
 				<div class="col-sm-6 solid">
 					<a href="<?php echo home_url('/'); ?>">
-						<h1>Martini Claro</h1>
-						<h3>Advogados Associados</h3>
-						<hr>
-						<p>Advocacia empresarial & Internacional</p>
+						<img id="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" />
 					</a>
 				</div>
 				<div class="col-sm-6 shadow">
